dd2efd0c07a82d80bca04e039ba0fa6fb7a22268
Noam Saar <noamsaar11@gmail.com>
2024-01-25 14:48:25 +0200

    Add CommentEdit and basic styling

--- src/cmps/CommentEdit.jsx
+++ src/cmps/CommentEdit.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react"
 
 
-export function CommentEdit() {
+export function CommentEdit({ onAddComment }) {
+    const [email, setEmail] = useState('')
+    const [txt, setTxt] = useState('')
+
+    function handleChange({ target }) {
+        const field = target.name
+        let value = target.value
+
+        switch (field) {
+            case 'email':
+                setEmail(value)
+                break
+            case 'txt':
+                setTxt(value)
+                break
+            default:
+                break
+        }
+    }
+
+    function handleSubmit(ev) {
+        ev.preventDefault()
+        onAddComment(email, txt)
+        setEmail('')
+        setTxt('')
+    }
+
     return (
         <section className="comment-edit">
-            hi from edit
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                />
+
+                <textarea
+                    name="txt"
+                    value={txt}
+                    onChange={handleChange}
+                    placeholder="Message"
+                ></textarea>
+
+                <button type="submit">SUBMIT</button>
+            </form>
         </section>
     )
 }
