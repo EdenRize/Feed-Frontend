1ef23f888a2024ca5a86d1b121e553f829ea0618
EdenRize <edenrize@gmail.com>
2024-01-25 13:26:49 +0200

    Init

---
+++ src/RootCmp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+import 'animate.css';
+import { HomePage } from './views/HomePage';
+
+
+
+export function RootCmp() {
+    return (
+        <section className="app">
+            <Routes>
+                <Route element={<HomePage />} path="/" />
+            </Routes>
+        </section>
+    )
+}
+
+
